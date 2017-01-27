a87eae54b13b315b362735e3e0cdcb49e9bfb74e
Travis Luong <travis@travisluong.com>
2017-01-27 13:54:34 -0800

    Temp deployment

---
+++ docs/js/build-config.js
@@ -0,0 +1,7 @@
+angular.module('docsApp').constant('BUILDCONFIG', {
+  "ngVersion": "1.5.5",
+  "version": "1.1.1",
+  "repository": "https://github.com/angular/material",
+  "commit": "38f5dcdbca13b2f7613f4f2f864bf37be7f6c9c3",
+  "date": "2017-01-19 21:20:37 -0800"
+});
